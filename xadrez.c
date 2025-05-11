795faedd31ba1754a5bd49c837d3c789f73f0c47
Gustavo Lima Machado <146608816+FireFox-exe@users.noreply.github.com>
2025-05-11 15:24:52 +0000

    correção movimentação do cavalo

--- xadrez.c
+++ xadrez.c
@@ -7,33 +7,38 @@ int main() {
    const int bispo = 5; // O Bispo vai se mover 5 casas na diagonal
    const int rainha = 8; // A Rainha vai se mover 8 casas para a esquerda
    const int torre = 5; // A Torre vai se mover 5 casas para a direita
-   const int cavalo; // Movimentação do cavalo em formato de loop aninhado. 2 para esquerda e 1 na perpendicular.
 
     //Torre - 5 casas para a direita
-    printf("movimentação da Torre\n");
+    printf("\nmovimentação da Torre\n");
     for (int mov = 0; mov < torre; mov++){
         printf("\nDireita\n");
+    }
 
     // Bispo - 5 casas na diagonal superior direita (Cima + Direita)
-    printf("Movimenação do Bispo\n");
+    printf("\nMovimenação do Bispo\n");
     for(int mov = 0; mov < bispo; mov++){
         printf("\nDiagonal direita\n");
     }
-    printf("Movimentação da Rainha\n");
 
     //Rainha - 8 casas para a esquerda
+    printf("\nMovimentação da Rainha\n");
     for (int mov = 0; mov < rainha;mov++){ 
         printf("\nEsquerda\n");
     }
 
-    //Cavalo - 2 passos para a Direita e 1 passo para Perpendicular.
-    for (int i = 0; i < 2; i++){
-        printf("Direita\n"); // ou esquerda
+    //Cavalo - 2 passos para a Direita e 2 passo para Perpendicular.
+    //horizontal
+    printf("\nMovimenação cavalo\n");
+    for (int i = 0; i < 1; i++) { //
+    for (int j = 0; j < 2; j++) {
+        printf("Direita\n");
     }
-        int j = 0;
-        while(j < 1){
-            printf("Cima\n"); // ou baixo
-        j++;
+        //vertical
+        int h = 0;
+        while (h < 2) {
+            printf("Cima\n");
+            h++;
+        }
     }
 
     return 0;
