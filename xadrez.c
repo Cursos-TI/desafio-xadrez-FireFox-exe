b938c9bc0875b922901b90586122bf4bf869e91c
Gustavo Lima Machado <146608816+FireFox-exe@users.noreply.github.com>
2025-05-11 15:00:21 +0000

    Cavalo loop externo horizontal,interno vertical

--- xadrez.c
+++ xadrez.c
@@ -7,6 +7,7 @@ int main() {
    const int bispo = 5; // O Bispo vai se mover 5 casas na diagonal
    const int rainha = 8; // A Rainha vai se mover 8 casas para a esquerda
    const int torre = 5; // A Torre vai se mover 5 casas para a direita
+   const int cavalo; // Movimentação do cavalo em formato de loop aninhado. 2 para esquerda e 1 na perpendicular.
 
     //Torre - 5 casas para a direita
     printf("movimentação da Torre\n");
@@ -23,6 +24,16 @@ int main() {
     //Rainha - 8 casas para a esquerda
     for (int mov = 0; mov < rainha;mov++){ 
         printf("\nEsquerda\n");
+    }
+
+    //Cavalo - 2 passos para a Direita e 1 passo para Perpendicular.
+    for (int i = 0; i < 2; i++){
+        printf("Direita\n"); // ou esquerda
+    }
+        int j = 0;
+        while(j < 1){
+            printf("Cima\n"); // ou baixo
+        j++;
     }
 
     return 0;
